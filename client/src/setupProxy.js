0026ace481e44bb8842b947cc634e35f4f4d9ba7
Luis Osta <luisosta1999@gmail.com>
2019-05-18 13:50:01 -0500

    removed package proxy with packagejson option

--- client/src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const proxy = require("http-proxy-middleware");
-
-module.exports = function(app) {
-  //app.use(proxy("/api/route", {target:  "http://localhost:PORT"})) <--- update for each API route
-};
