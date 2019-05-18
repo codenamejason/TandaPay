ba68702848036bbf864869b2f75443d6b7c43403
Luis Osta <luisosta1999@gmail.com>
2019-05-18 13:47:26 -0500

    added basis for react/express proxy

---
+++ client/src/setupProxy.js
@@ -0,0 +1,5 @@
+const proxy = require("http-proxy-middleware");
+
+module.exports = function(app) {
+  //app.use(proxy("/api/route", {target:  "http://localhost:PORT"})) <--- update for each API route
+};
