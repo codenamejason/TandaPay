367737120872a164d7a7b5af5783555cd1341589
Luis Osta <luisosta1999@gmail.com>
2019-05-31 14:07:19 -0500

    added facebook configs

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN
