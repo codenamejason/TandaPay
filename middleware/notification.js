d9ab4c6911f3fe74a839a83a8725bbfb712d0674
Will Franzen <wtfranzen@gmail.com>
2019-06-03 18:48:51 -0500

    Add a doc comment to every function
    as assigned

--- middleware/notification.js
+++ middleware/notification.js
@@ -1,6 +1,15 @@
 let Notifications = require("../lib/notification.js");
 
 module.exports = sendNotificationsMiddleware;
+/**
+ * @summary: A middleware to send notifications. Behavior defined by the
+ *           notificationKind prop on the req object hopefully set by a previous
+ *           controller
+ * @param req: The Express request object
+ * @param res: The Express response object
+ * @param next: The Express next function
+ * @returns: void
+ */
 function sendNotificationsMiddleware(req, res, next) {
     let { tandaID, notificationKind } = req;
     let notification = createNotification(notificationKind, req);
@@ -12,6 +21,12 @@ function sendNotificationsMiddleware(req, res, next) {
     next();
 }
 
+/**
+ * @summary: Instantiates the correct subclass of Notification specified by kind
+ * @param kind: The Notification.prototype.kind to match against
+ * @param req: The Express request object
+ * @returns: void
+ */
 function createNotification(kind, req) {
     let NotificationImplementation = Object.values(Notifications).find(
         clazz => clazz.prototype.kind == kind
