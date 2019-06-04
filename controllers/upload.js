55fb97a02376e0447e3e67fe766983bac8511702
Will Franzen <wtfranzen@gmail.com>
2019-06-04 13:29:57 -0500

    Create lib/storage.js to do storagr related things

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,10 +1,5 @@
-let uuid = require('uuid/v4')
-let { Storage } = require("@google-cloud/storage");
-
-const FIFTEEN_MINUTES = 15 * 60 * 1000;
-
-let storage = new Storage();
-let bucket = storage.bucket(process.env.GOOGLE_BUCKET_NAME);
+let uuid = require("uuid/v4");
+let { createUploadUrl } = require("../lib/storage.js");
 
 /**
  * @summary: Upload controller...
@@ -15,12 +10,8 @@ let bucket = storage.bucket(process.env.GOOGLE_BUCKET_NAME);
  */
 async function uploadController(req, res, next) {
     let name = uuid();
-    let file = bucket.file(name);
-    let url = (await file.getSignedUrl({
-        action: "write",
-        expires: Date.now() + FIFTEEN_MINUTES,
-    }))[0];
-    res.status(200).send(url + '\n');
+    let url = await createUploadUrl(name);
+    res.status(200).send(url + "\n");
 }
 
 module.exports = uploadController;
