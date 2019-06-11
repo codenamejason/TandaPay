7420cae469abe924cfac893774e3eda9c3abbdc7
Luis Osta <luisosta1999@gmail.com>
2019-06-11 18:12:00 -0500

    added selected styling

--- client/src/Navigation/SideBar/components/menuButtons.js
+++ client/src/Navigation/SideBar/components/menuButtons.js
@@ -6,19 +6,32 @@ import {
 	ListItemIcon,
 	ListItemText
 } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { Link, withRouter } from "react-router-dom";
 import { nav, extra } from "./buttonConfig";
-import styles from "../sidebar.style";
+import styles from "./buttons.style";
 const menuButtons = (props) => {
 	const { classes, type } = props;
 	const menuItems = type === "nav" ? nav.menuItems : extra.menuItems;
 	const itemProps = type === "nav" ? nav.itemProps : extra.itemProps;
-
+	const path = props.location.pathname.split("/");
+	const routeSelected = path[2] === undefined ? "" : path[2];
+	console.log(routeSelected);
 	return (
 		<List>
 			{menuItems.map((text, index) => (
-				<Link to={`/admin/${itemProps[text].url}`} className={classes.link}>
-					<ListItem button key={text}>
+				<Link
+					to={`/admin/${itemProps[text].url}`}
+					className={classes.link}
+					key={text}
+				>
+					<ListItem
+						button
+						key={text}
+						className={clsx({
+							[classes.selected]: itemProps[text].url === routeSelected
+						})}
+					>
 						<ListItemIcon>{itemProps[text].icon}</ListItemIcon>
 						<ListItemText primary={text} />
 					</ListItem>
@@ -27,4 +40,4 @@ const menuButtons = (props) => {
 		</List>
 	);
 };
-export default withStyles(styles, { withTheme: true })(menuButtons);
+export default withRouter(withStyles(styles, { withTheme: true })(menuButtons));
