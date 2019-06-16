d00dea008c10eac3528efbe39ed3e8351633be52
Luis Osta <luisosta1999@gmail.com>
2019-06-16 13:42:54 -0500

    refactored configs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
-
+const keys = require("./config/keys");
 const PORT = process.env.PORT || 8080;
-const { MONGO_URI } = process.env;
+const MONGO_URI = keys.mongoURI;
 
 require("./models/register");
 require("mongoose").connect(MONGO_URI, { useNewUrlParser: true });
