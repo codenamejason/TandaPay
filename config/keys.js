b03d6dd354a2e0ea87cefdf8f81df28618ba7d44
Luis Osta <luisosta1999@gmail.com>
2019-06-24 21:14:42 -0500

    added ci to prod env

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV === "production") {
-  // we are in production
-  module.exports = require("./prod");
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "ci") {
+    // we are in production
+    module.exports = require("./prod");
 } else {
-  // We are in development or test environment
-  module.exports = require("./dev");
+    // We are in development or test environment
+    module.exports = require("./dev");
 }
