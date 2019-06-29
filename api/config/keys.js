885e966afbc636d99dfb120feda4c676255b0bd8
Luis Osta <luisosta1999@gmail.com>
2019-06-29 10:32:07 -0500

    first attempt at deployment

--- api/config/keys.js
+++ api/config/keys.js
@@ -1,6 +1,8 @@
 if (process.env.NODE_ENV === "production") {
     // we are in production
-    module.exports = require("./prod");
+    module.exports = require("./env");
+} else if (process.env.NODE_ENV === "ci") {
+    module.exports = require("./ci");
 } else if (process.env.NODE_ENV === "test") {
     // we are in test
     module.exports = require("./test");
