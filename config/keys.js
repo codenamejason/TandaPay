87c982131bbc9561462c60c27d6e99b717d48d37
Luis Osta <luisosta1999@gmail.com>
2019-06-28 20:35:47 -0500

    changed key handling

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "ci") {
+if (process.env.NODE_ENV === "production") {
     // we are in production
-    module.exports = require("./prod");
+    module.exports = require("./env");
+} else if (process.env.NODE_ENV === "ci") {
+    module.exports = require("./ci");
 } else {
     // We are in development or test environment
     module.exports = require("./dev");
