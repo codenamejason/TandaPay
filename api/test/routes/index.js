ca5bda7d716533d4d49fad4bf4767263e3d10665
Will Franzen <wtfranzen@gmail.com>
2019-07-03 13:28:02 -0600

    Add commit info to /version

--- api/test/routes/index.js
+++ api/test/routes/index.js
@@ -4,10 +4,10 @@ let { setupAll, sleep } = require("../common");
 let { fake, http, data } = setupAll(test);
 
 test("server starts (and reports version)", async t => {
-    let { version } = require("../../package.json");
     let res = await http().get("/version");
 
     t.is(res.statusCode, 200);
-    t.is(res.text, version);
-    t.regex(res.header["content-type"], /text/);
+    t.regex(res.header["content-type"], /json/);
+    t.truthy(res.body.version);
+    t.truthy(res.body.commit);
 });
