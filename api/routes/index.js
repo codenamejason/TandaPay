6ca35e19249ab7d73b9cf32b24bd04cf244a575a
William Franzen <wtfranzen@gmail.com>
2019-07-08 17:39:26 -0600

    More cors fixes

--- api/routes/index.js
+++ api/routes/index.js
@@ -12,6 +12,7 @@ app.use(
             "http://127.0.0.1:3000",
         ],
         allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
     })
 );
 app.use(require("cookie-parser")());
