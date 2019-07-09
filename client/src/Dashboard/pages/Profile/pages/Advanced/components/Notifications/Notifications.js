008df24b81393712c344260aa1ec79c4db8da712
Will Franzen <wtfranzen@gmail.com>
2019-07-09 16:02:44 -0600

    Connect group dashboard with api backend

--- client/src/Dashboard/pages/Profile/pages/Advanced/components/Notifications/Notifications.js
+++ client/src/Dashboard/pages/Profile/pages/Advanced/components/Notifications/Notifications.js
@@ -14,16 +14,17 @@ import {
 import axios from "axios";
 
 import styles from "./notifications.style";
+import { API_BASE } from "../config.json";
 
 class Notifications extends React.Component {
     constructor(props) {
         super(props);
 
         this.api = axios.create({
-            baseURL: "http://localhost:8080",
+            baseURL: API_BASE,
             headers: {
                 Authorization:
-                    "Bearer " + document.cookie.replace("x-auth=", ""),
+                    "Bearer " + document.cookie.match(/x-auth=(\S+);/)[1],
             },
         });
 
--- client/src/actions/group.js
+++ client/src/actions/group.js
@@ -1,15 +1,27 @@
 import axios from "axios";
 import { FETCH_GROUP } from "./types";
-const API_BASE = process.env.REACT_APP_API_BASE;
+import { API_BASE } from "../config.json";
+
+/**
+ * @summary generates the options to use with a request. this is a function
+ *          and not a const because the document.cookie may change
+ */
+function config() {
+    return {
+        baseURL: "http://localhost:8080",
+        headers: {
+            Authorization:
+                "Bearer " + document.cookie.match(/x-auth=(\S+);/)[1],
+        },
+    };
+}
 
 /**
  * @summary Redux action creator to fetch the user's group
  */
 export const fetchGroup = groupID => async dispatch => {
     try {
-        const response = await axios.get(API_BASE + "/groups/" + groupID, {
-            withCredentials: true,
-        });
+        const response = await axios.get("/groups/" + groupID, config());
 
         dispatch({ type: FETCH_GROUP, payload: response.data });
     } catch (err) {
@@ -23,9 +35,9 @@ export const fetchGroup = groupID => async dispatch => {
 export const createGroup = ({ name, premium }) => async dispatch => {
     try {
         const response = await axios.post(
-            API_BASE + "/groups/new",
+            "/groups/new",
             { groupName: name, premium },
-            { withCredentials: true }
+            config()
         );
 
         dispatch({ type: FETCH_GROUP, payload: response.data });
