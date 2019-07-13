e750c7cf461f557de9aff6c064e552f856b3c53c
Luis Osta <luisosta1999@gmail.com>
2019-07-13 00:06:12 -0500

    added basic loading screen for base route

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,9 @@ import { ThemeProvider } from "@material-ui/styles";
 import * as actions from "./actions/index";
 //HOCs
 import {
-	withAuthorization,
-	withoutAuthorization,
-	withIncompleteAuthorization
+    withAuthorization,
+    withoutAuthorization,
+    withIncompleteAuthorization,
 } from "./HOCs/authorized";
 //
 
@@ -17,34 +17,59 @@ import Dashboard from "./Dashboard/Dashboard";
 import Setup from "./Setup/Setup";
 
 import theme from "./theme";
+import { Typography } from "@material-ui/core";
 class App extends React.Component {
-	componentDidMount() {
-		this.props.fetchUser();
-	}
-	render() {
-		return (
-			<ThemeProvider theme={theme}>
-				<BrowserRouter>
-					<Switch>
-						<Route
-							exact
-							path="/"
-							component={withoutAuthorization(Registration)}
-						/>
-						<Route
-							exact
-							path="/setup"
-							component={withIncompleteAuthorization(Setup)}
-						/>
-						<Route path="/admin" component={withAuthorization(Dashboard)} />
-					</Switch>
-				</BrowserRouter>
-			</ThemeProvider>
-		);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+        };
+    }
+    async componentDidMount() {
+        try {
+            await this.props.fetchUser();
+            this.setState({
+                loading: false,
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    render() {
+        const { loading } = this.state;
+        if (loading) {
+            return (
+                <div>
+                    <Typography variant="h1">Loading ...</Typography>
+                </div>
+            );
+        }
+        return (
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                            component={withoutAuthorization(Registration)}
+                        />
+                        <Route
+                            exact
+                            path="/setup"
+                            component={withIncompleteAuthorization(Setup)}
+                        />
+                        <Route
+                            path="/admin"
+                            component={withAuthorization(Dashboard)}
+                        />
+                    </Switch>
+                </BrowserRouter>
+            </ThemeProvider>
+        );
+    }
 }
 
 export default connect(
-	null,
-	actions
+    null,
+    actions
 )(App);
