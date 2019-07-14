706379f39fa6ae346361f2e5cf336367c11c834f
Luis Osta <luisosta1999@gmail.com>
2019-07-14 18:22:43 -0500

    added helmet and CSP for backend

--- client/src/web3/index.js
+++ client/src/web3/index.js
@@ -9,37 +9,36 @@ const fm = new Fortmatic(process.env.REACT_APP_FORTMATIC_ID);
  * @global
  */
 const attemptConnection = async user => {
-    const { walletProvider } = user;
-    console.log(user);
-    if (walletProvider === "metamask") {
-        connectToMetamask();
-    } else {
-        connectToFortmatic();
-    }
+  const { walletProvider } = user;
+  console.log(user);
+  if (walletProvider === "metamask") {
+    connectToMetamask();
+  } else {
+    connectToFortmatic();
+  }
 };
 /**
  * @summary
  * @global
  */
 const connectToMetamask = async () => {
-    if (window.ethereum) {
-        console.log(Web3);
-        window.web3 = new Web3(window.ethereum);
-        try {
-            const accounts = await window.ethereum.enable();
-            console.log("Metamask Web3 function: ", accounts);
-            return [accounts, null];
-        } catch (error) {
-            //User rejected account access
-            return [null, "There's been an error"];
-        }
-    } else if (window.web3) {
-        window.web3 = new Web3(window.web3.currentProvider);
-        return ["success", null];
-    } else {
-        //account doesn't have metamask
-        return [null, "There's been an error"];
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    try {
+      const accounts = await window.ethereum.enable();
+      console.log("Metamask Web3 function: ", accounts);
+      return [accounts, null];
+    } catch (error) {
+      //User rejected account access
+      return [null, "There's been an error"];
     }
+  } else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider);
+    return ["success", null];
+  } else {
+    //account doesn't have metamask
+    return [null, "There's been an error"];
+  }
 };
 
 /**
@@ -47,15 +46,15 @@ const connectToMetamask = async () => {
  * @global
  */
 const connectToFortmatic = async () => {
-    window.web3 = new Web3(fm.getProvider());
+  window.web3 = new Web3(fm.getProvider());
 
-    try {
-        const accounts = await window.web3.currentProvider.enable();
-        return [accounts, null];
-    } catch (error) {
-        console.log(error);
-        return [null, error];
-    }
+  try {
+    const accounts = await window.web3.currentProvider.enable();
+    return [accounts, null];
+  } catch (error) {
+    console.log(error);
+    return [null, error];
+  }
 };
 
 /**
@@ -63,23 +62,23 @@ const connectToFortmatic = async () => {
  * @global
  */
 const currentProvider = () => {
-    //if web3 in the window isn't defined
-    if (!window.web3) {
-        return "";
+  //if web3 in the window isn't defined
+  if (!window.web3) {
+    return "";
+  }
+  try {
+    const currentProvider = window.web3.currentProvider;
+    let wallet = "";
+    if (currentProvider.isFortmatic) {
+      wallet = "fortmatic";
+    } else if (currentProvider.host === "metamask") {
+      wallet = "metamask";
     }
-    try {
-        const currentProvider = window.web3.currentProvider;
-        let wallet = "";
-        if (currentProvider.isFortmatic) {
-            wallet = "fortmatic";
-        } else if (currentProvider.host === "metamask") {
-            wallet = "metamask";
-        }
 
-        return wallet;
-    } catch (error) {
-        return "";
-    }
+    return wallet;
+  } catch (error) {
+    return "";
+  }
 };
 
 /**
@@ -88,25 +87,25 @@ const currentProvider = () => {
  *
  */
 const getDAIBalance = async () => {
-    //
-    try {
-        const web3 = window.web3;
-        const instance = new web3.eth.Contract(
-            DaiContract.abi,
-            process.env.REACT_APP_DAI_ADDRESS
-        );
-        const accounts = await web3.eth.getAccounts();
-        const balance = await instance.methods.balanceOf(accounts[0]).call();
-        return [balance, null];
-    } catch (e) {
-        return [null, e];
-    }
+  //
+  try {
+    const web3 = window.web3;
+    const instance = new web3.eth.Contract(
+      DaiContract.abi,
+      process.env.REACT_APP_DAI_ADDRESS
+    );
+    const accounts = await web3.eth.getAccounts();
+    const balance = await instance.methods.balanceOf(accounts[0]).call();
+    return [balance, null];
+  } catch (e) {
+    return [null, e];
+  }
 };
 
 export {
-    connectToMetamask,
-    connectToFortmatic,
-    currentProvider,
-    attemptConnection,
-    getDAIBalance,
+  connectToMetamask,
+  connectToFortmatic,
+  currentProvider,
+  attemptConnection,
+  getDAIBalance,
 };
