f4bb837e6eeb9400d217646fefe845aaa530edda
Luis Osta <luisosta1999@gmail.com>
2019-07-13 19:16:59 -0500

    added spacing to recent claims

--- client/src/Dashboard/pages/Claims/components/RecentClaims/RecentClaims.js
+++ client/src/Dashboard/pages/Claims/components/RecentClaims/RecentClaims.js
@@ -7,7 +7,7 @@ import styles from "./recent.style.js";
 const RecentClaims = props => {
   const { classes, claims } = props;
   return (
-    <Grid container className={classes.claimContainer}>
+    <Grid container className={classes.claimContainer} spacing={2}>
       {claims.map((claim, index) => (
         <Grid item xs={12} sm={6} md={3} key={index}>
           <ClaimCard claim={claim} />
