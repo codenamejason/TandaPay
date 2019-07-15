a8fdab8d17f4ce400a8ba9fd136fb6a29ab85298
Will Franzen <wtfranzen@gmail.com>
2019-07-15 12:24:16 -0600

    Return groupID as part of user's profile

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -69,6 +69,7 @@ const sendProfile = (req, res) => {
         phone,
         ethereumAddress,
         settings,
+        groupID,
     } = user;
     return res.send({
         token,
@@ -82,6 +83,7 @@ const sendProfile = (req, res) => {
         phone,
         ethereumAddress,
         settings,
+        groupID,
     });
 };
 module.exports = {
